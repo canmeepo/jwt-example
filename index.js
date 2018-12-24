1ddc6f86e4445ffa71e760e7bbaa1f0001d897bc
canmeepo <canmeepo@yandex.ru>
2018-12-24 19:51:32 +0300

    add auth

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 8888;
 
-app.get("/status", (req, res) => {
-    const localTime = (new Date()).toLocaleDateString();
+const usersMock = [
+    {id: 1, name: 'admin', password: 'admin'},
+    {id: 2, name: 'root', password: 'root'}
+]
 
-    res.status(200).send(`Server time is ${localTime}`)
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/signin", (req, res) => {
+    if (!req.body.name || !req.body.password) {
+        res.status(400).send('not vaolid');
+
+        return;
+    }
+
+    const user = usersMock.find(x => {
+        return x.name === req.body.name && x.password === req.body.password;
+    })
+
+    if (!user) {
+        res.status(401).send('user not found');
+
+        return;
+    }
+
+    const token = jwt.sign({
+        sub: user.id,
+        name: user.name
+    }, 'salt', {expiresIn: "3 hours"});
+
+    res.status(200).send({authtoken: token});
+})
+
+app.get("/public", (req, res) => {
+    res.status(200).send('public api')
+})
+
+app.get("/public", (req, res) => {
+    res.status(200).send('public api')
 })
 
 app.get("*", (req, res) => {
-    res.status(404)
+    res.status(404).send('not foudn')
 })
 
 app.listen(PORT, () => {
